870710b384f3d98a9b6f532c5ceb8095c87aa00f
3nsar <84131267+3nsar@users.noreply.github.com>
2024-08-07 21:35:55 +0200

    commit

--- image-gallery/src/App.jsx
+++ image-gallery/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 const App = () => {
+  
+  const [images, setImages] = useState([])
+  const url = "https://picsum.photos/v2/list?page=2&limit=10"
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const res = await axios.get(url)
+      setImages(res.data)
+      console.log(res.data)
+    }
+    fetchData()
+  },[]) 
+
   return (
     <div>
       <h1>Hoc est porticus</h1>
+      {images.map((image)=>(
+        <div className='img-container'>
+           <img src={image.download_url} alt="pic" height="200px"/>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
